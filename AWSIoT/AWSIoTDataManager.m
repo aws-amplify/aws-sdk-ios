493fbebcf9928fa9cafa89fa5cd2b89b53d56dd1
Ram <ram1@Ramakrishnans-MacBook-Pro.local>
2017-02-24 09:24:28 +0530

    Fix issue reported in Shadow Operation Still In Progress! BUG! #565

--- AWSIoT/AWSIoTDataManager.m
+++ AWSIoT/AWSIoTDataManager.m
@@ -713,7 +713,7 @@ static void (^shadowMqttMessageHandler)(NSObject *mqttClient, NSString *topic, N
             // Start the shadow operation timer.
             //
             shadow.timer = [NSTimer timerWithTimeInterval:shadow.operationTimeout target:self selector: @selector(shadowOperationTimeoutOnTimer:) userInfo:name repeats:NO];
-            
+            [[NSRunLoop mainRunLoop] addTimer:shadow.timer forMode:NSRunLoopCommonModes];
             //
             // Add the version number (if known and versioning is enabled) and
             // client token properties to the state dictionary.
