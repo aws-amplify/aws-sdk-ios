52195f4614884933bf5adb92a0dae9caf653bc67
Wayne Mock <wemock@mac.com>
2018-03-05 13:05:53 -0600

    Initial AWSTranscribe commit

---
+++ AWSTranscribe/AWSTranscribe.h
@@ -0,0 +1,19 @@
+//
+//  AWSTranscribe.h
+//  AWSTranscribe
+//
+//  Created by Wayne on 3/5/18.
+//  Copyright © 2018 Amazon Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AWSTranscribe.
+FOUNDATION_EXPORT double AWSTranscribeVersionNumber;
+
+//! Project version string for AWSTranscribe.
+FOUNDATION_EXPORT const unsigned char AWSTranscribeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWSTranscribe/PublicHeader.h>
+
+
