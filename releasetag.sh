fbe8a1a9f2a0b8272ec6b0a3351f2a47ea3e3614
Chunqiang Sun <sdechunq@amazom.com>
2018-11-04 01:04:25 -0700

    update circleci config

--- releasetag.sh
+++ releasetag.sh
@@ -1,8 +1,3 @@
-echo "hello"
-tag="${CIRCLE_TAG}"
-
 tagdescription=$(sed -n '/## '${CIRCLE_TAG}'/,/## [0-9]*\.[0-9]*\.[0-9]/p'  CHANGELOG.md | sed '1d' | sed '$d')
 tagname="AWS SDK for iOS "${CIRCLE_TAG}
-echo $tagname
-echo "$tagdescription"
-
+github-release release -s ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME}  -t ${CIRCLE_TAG} -d "$tagdescription" -n "$tagname"
