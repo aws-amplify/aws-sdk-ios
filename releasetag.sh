87881160c2f8a1506dd821c727ce12e81160cd76
Chunqiang Sun <sdechunq@amazom.com>
2018-11-04 00:44:54 -0700

    g

--- releasetag.sh
+++ releasetag.sh
@@ -1,6 +1,6 @@
 echo "hello"
-echo "${CIRCLE_TAG}"
-echo '/## ${CIRCLE_TAG}/,/## [0-9]*\.[0-9]*\.[0-9]/p'
+tag="${CIRCLE_TAG}"
+echo '/## $tag/,/## [0-9]*\.[0-9]*\.[0-9]/p'
 tagdescription=$(sed -n '/## ${CIRCLE_TAG}/,/## [0-9]*\.[0-9]*\.[0-9]/p'  CHANGELOG.md)
 tagname="AWS SDK for iOS "${CIRCLE_TAG}
 echo $tagname
