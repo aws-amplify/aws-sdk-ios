c90b12413deba7777c2c2fafdc3b3b21ac940264
Chunqiang Sun <sdechunq@amazom.com>
2018-11-04 22:48:27 -0800

    update circleci config

--- cocoapods_release.py
+++ cocoapods_release.py
@@ -1,7 +1,11 @@
 from subprocess import Popen, PIPE
+import subprocess
 import os
 from datetime import datetime
 import time
+
+
+
 podpackages = [
                'AWSCore.podspec',
                'AWSAPIGateway.podspec',
@@ -51,48 +55,23 @@ podpackages = [
 print (str(datetime.now()) + ': publishing cocoapods ...')
 for package in podpackages:
     print (str(datetime.now())+': publishing ' + package + ' ...')
-    process = Popen(["pod", 'trunk','push',package,'--allow-warnings'], stdout=PIPE, stderr=PIPE)
-    #process = Popen(["pod", 'repo','push','https://github.com/sunchunqiang/mypod-specs',package,'--allow-warnings'], stdout=PIPE)
-    #  (output, err) = process.communicate()
-    output = process.stdout.readline()
-    err = process.stderr.readline();
+    process = Popen(["pod", 'trunk','push',package,'--allow-warnings'], stdout= PIPE, stderr= PIPE)
     wait_times = 0 ;
-    exit_code = process.poll()
-    while exit_code is None:
-        wait_times = wait_times + 1;
-        if wait_times % 50 == 0 :
-            print(str(datetime.now())+ ": I am still alive")
-        if wait_times > 600 :
-            print(str(datetime.now())+ ": time out")
-            quit(1)
-        
-        time.sleep(10)
-        
-        outputline = process.stdout.readline()
-        while outputline !='':
-            output = output +  outputline
-            #  print(outputline)
-            outputline = process.stdout.readline()
+    while True:
+        try:
+            (output, err) = process.communicate(timeout = 10)
+        except subprocess.TimeoutExpired:
+            wait_times = wait_times + 1;
+            #tell circleci I am still alive, don't kill me
+            if wait_times % 30 == 0 :
+                print(str(datetime.now())+ ": I am still alive")
+            if wait_times > 600 :
+                print(str(datetime.now())+ ": time out")
+                quit(1)
 
-        errline = process.stderr.readline()
-        while outputline !='':
-            err = errline +  outputline
-            #            print(errline)
-            errline = process.stderr.readline()
-
-        exit_code = process.poll()
-
-    outputline = process.stdout.readline()
-    while outputline !='':
-        output = output +  outputline
-        outputline = process.stdout.readline()
-        
-    errline = process.stderr.readline()
-    while outputline !='':
-        err = errline +  outputline
-        errline = process.stderr.readline()
-#  (output, err) = process.communicate()
-#   print("output:" + str(output))
+            continue
+        break
+    exit_code = process.wait()
     if exit_code != 0 :
         if "Unable to accept duplicate entry for:" in str(output):
             print (str(datetime.now()) +": " +  package +" is already published")
@@ -102,4 +81,7 @@ for package in podpackages:
             print(str(datetime.now()) + " Failed to publish " + package)
             quit(exit_code);
     print (str(datetime.now())+': published ' + package)
-  
+
+
+
+
