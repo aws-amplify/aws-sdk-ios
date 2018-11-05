c088fe440d8e7cea8b2dddd4ae1fcb2a0cd3d3fd
Chunqiang Sun <sdechunq@amazom.com>
2018-11-04 16:52:24 -0800

    update circleci config

--- cocoapods_release.py
+++ cocoapods_release.py
@@ -1,6 +1,8 @@
 from subprocess import Popen, PIPE
 import os
-podpackages = [  
+from datetime import datetime
+import time
+podpackages = [
                'AWSCore.podspec',
                'AWSAPIGateway.podspec',
                'AWSAutoScaling.podspec',
@@ -46,19 +48,30 @@ podpackages = [
                'AWSiOSSDKv2.podspec',
 
                ]
+print (str(datetime.now()) + ': publishing cocoapods ...')
 for package in podpackages:
-    print ('publishing ' + package + ' ...')
+    print (str(datetime.now())+': publishing ' + package + ' ...')
     process = Popen(["pod", 'trunk','push',package,'--allow-warnings'], stdout=PIPE)
     #process = Popen(["pod", 'repo','push','https://github.com/sunchunqiang/mypod-specs',package,'--allow-warnings'], stdout=PIPE)
+    ##  (output, err) = process.communicate()
+    wait_times = 0 ;
+    while process.poll() is None:
+        wait_times = wait_times + 1;
+        if wait_times % 5 == 0 :
+            print(str(datetime.now())+ ": I am still alive")
+        if wait_times > 600 :
+            print(str(datetime.now())+ ": time out")
+            quit(1)
+        time.sleep(10)
+    exit_code = process.poll()
     (output, err) = process.communicate()
-    exit_code = process.wait()
     if exit_code != 0 :
         if "Unable to accept duplicate entry for:" in str(output):
-            print (package +" is already published")
+            print (str(datetime.now()) +": " +  package +" is already published")
         else:
             print(output)
             print(err,  exit_code  )
-            print("Failed to publish " + package)
+            print(str(datetime.now()) + " Failed to publish " + package)
             quit(exit_code);
-    print ('published ' + package)
+    print (str(datetime.now())+': published ' + package)
   
