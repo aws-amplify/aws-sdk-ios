2e326a446779af5ab61ff8386625b1aa92a5cfb9
Chunqiang Sun <sdechunq@amazom.com>
2018-11-06 10:42:18 -0800

    update circleci config

---
+++ CreateCarthageArchive.sh
@@ -0,0 +1,14 @@
+frameworkfilename=$(carthage archive |  grep -o 'Created .*\.zip$'  | grep -o   '\S*\.zip$')
+
+if [ -z  "$frameworkfilename" ];then
+	echo "Cannot find framework zip file from output of carthage archive"
+	exit 1
+fi
+
+if [ ! -f "$frameworkfilename" ]; then
+	echo "$frameworkfilename is not present"
+	exit 1
+fi
+
+mv $frameworkfilename   aws-sdk-ios-carthage.framework.zip
+
