85cd7ff9dda039d25da942f39deeed7c8bfb3c7e
sunchunqiang <sdechunq@amazon.com>
2019-01-24 16:23:24 -0800

    upload sdk to s3

---
+++ CircleciScripts/copy_resourcefiles.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from functions import runcommand
+from shutil import copyfile
+
+root = sys.argv[1]
+dest = sys.argv[2]
+files = {
+"LICENSE.AMAZON":"LICENSE.AMAZON",
+"LICENSE":"LICENSE",
+"LICENSE.APACHE":"LICENSE.APACHE",
+"NOTICE":"NOTICE",
+"README.md":"README.md",
+"CircleciScripts/src/README.html":"src/source.html",
+"CircleciScripts/samples/README.html":"samples/samples.html"
+}
+
+for source,target in files.items(): 
+    s = os.path.join(root, source)
+    t = os.path.join(dest, target)
+    runcommand("mkdir -p '{0}'".format(os.path.dirname(t)))
+    copyfile(s,t)
+
--- Scripts/Package.sh
+++ Scripts/Package.sh
@@ -18,31 +18,54 @@ then
 	exit 0
 fi
 
+rm -rf  builtFramework
 set -u
 
-if [ -x "Scripts/SdkPackage.sh" ]; then
-    Scripts/SdkPackage.sh AWSCore
-    Scripts/SdkPackage.sh AWSAutoScaling
-    Scripts/SdkPackage.sh AWSCognito
-    Scripts/SdkPackage.sh AWSCognitoIdentityProvider
-    Scripts/SdkPackage.sh AWSCognitoIdentityProviderASF
-    Scripts/SdkPackage.sh AWSCognitoAuth
-    Scripts/SdkPackage.sh AWSCloudWatch
-    Scripts/SdkPackage.sh AWSDeepSense
-    Scripts/SdkPackage.sh AWSDynamoDB
-    Scripts/SdkPackage.sh AWSEC2
-    Scripts/SdkPackage.sh AWSElasticLoadBalancing
-    Scripts/SdkPackage.sh AWSIoT
-    Scripts/SdkPackage.sh AWSKinesis
-    Scripts/SdkPackage.sh AWSKMS
-    Scripts/SdkPackage.sh AWSLambda
-    Scripts/SdkPackage.sh AWSLogs
-    Scripts/SdkPackage.sh AWSMachineLearning
-    Scripts/SdkPackage.sh AWSMobileAnalytics
-    Scripts/SdkPackage.sh AWSPinpoint
-    Scripts/SdkPackage.sh AWSS3
-    Scripts/SdkPackage.sh AWSSES
-    Scripts/SdkPackage.sh AWSSimpleDB
-    Scripts/SdkPackage.sh AWSSNS
-    Scripts/SdkPackage.sh AWSSQS
+if [ -f "Scripts/SdkPackage.sh" ]; then
+    chmod +x Scripts/SdkPackage.sh
+    Scripts/SdkPackage.sh   AWSCore
+    Scripts/SdkPackage.sh   AWSLex
+    Scripts/SdkPackage.sh   AWSLambda
+    Scripts/SdkPackage.sh   AWSLogs
+    Scripts/SdkPackage.sh   AWSMachineLearning
+    Scripts/SdkPackage.sh   AWSMobileAnalytics
+    Scripts/SdkPackage.sh   AWSPinpoint
+    Scripts/SdkPackage.sh   AWSPolly
+    Scripts/SdkPackage.sh   AWSRekognition
+    Scripts/SdkPackage.sh   AWSS3
+    Scripts/SdkPackage.sh   AWSSES
+    Scripts/SdkPackage.sh   AWSSNS
+    Scripts/SdkPackage.sh   AWSSQS
+    Scripts/SdkPackage.sh   AWSSimpleDB
+    Scripts/SdkPackage.sh   AWSTranscribe
+    Scripts/SdkPackage.sh   AWSTranslate
+        
+    Scripts/SdkPackage.sh   AWSAuthCore "$(pwd)/AWSAuthSDK/AWSAuthSDK.xcodeproj"
+    Scripts/SdkPackage.sh   AWSAuthUI   "$(pwd)/AWSAuthSDK/AWSAuthSDK.xcodeproj"
+    Scripts/SdkPackage.sh   AWSFacebookSignIn   "$(pwd)/AWSAuthSDK/AWSAuthSDK.xcodeproj"
+    Scripts/SdkPackage.sh   AWSGoogleSignIn "$(pwd)/AWSAuthSDK/AWSAuthSDK.xcodeproj"
+    Scripts/SdkPackage.sh   AWSMobileClient "$(pwd)/AWSAuthSDK/AWSAuthSDK.xcodeproj"
+    Scripts/SdkPackage.sh   AWSUserPoolsSignIn  "$(pwd)/AWSAuthSDK/AWSAuthSDK.xcodeproj"   
+
+    Scripts/SdkPackage.sh   AWSAPIGateway
+    Scripts/SdkPackage.sh   AWSAutoScaling
+    Scripts/SdkPackage.sh   AWSCloudWatch
+    Scripts/SdkPackage.sh   AWSCognito
+    Scripts/SdkPackage.sh   AWSCognitoAuth
+    Scripts/SdkPackage.sh   AWSCognitoIdentityProvider
+    Scripts/SdkPackage.sh   AWSCognitoIdentityProviderASF
+    Scripts/SdkPackage.sh   AWSComprehend
+    Scripts/SdkPackage.sh   AWSDynamoDB
+    Scripts/SdkPackage.sh   AWSEC2
+    Scripts/SdkPackage.sh   AWSElasticLoadBalancing
+    Scripts/SdkPackage.sh   AWSIoT
+    Scripts/SdkPackage.sh   AWSKMS
+    Scripts/SdkPackage.sh   AWSKinesis
+    Scripts/SdkPackage.sh   AWSKinesisVideo
+    Scripts/SdkPackage.sh   AWSKinesisVideoArchivedMedia
+
+else 
+    echo "$(pwd)"
+    echo "Did not find Scripts/SdkPackage.sh"
+    exit 1
 fi
