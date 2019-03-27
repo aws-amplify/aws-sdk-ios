a04f6e77e5e7694c02090e75d9f691c306dfd4de
James Ingham <james@ingh.am>
2019-03-27 14:53:50 +0000

    Hide the ‘signupButton’ instead of removing it completely to prevent the UI from breaking.

--- AWSAuthSDK/Sources/AWSAuthUI/AWSSignInViewController.m
+++ AWSAuthSDK/Sources/AWSAuthUI/AWSSignInViewController.m
@@ -269,7 +269,7 @@ static NSInteger const SCALED_DOWN_LOGO_IMAGE_HEIGHT = 140;
                                   action:@selector(handleUserPoolSignUp)
                         forControlEvents:UIControlEventTouchUpInside];
         } else {
-            [self.signUpButton removeFromSuperview];
+            [self.signUpButton setAlpha:0.0f];
         }
     } else {
         [self.tableFormView removeFromSuperview];
