d8b4173620d31c6cade1dd7a72c67b51efbe0caa
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2019-07-16 06:28:09 -0700

    [ec2] Update models to latest

--- AWSEC2UnitTests/AWSGeneralEC2Tests.m
+++ AWSEC2UnitTests/AWSGeneralEC2Tests.m
@@ -523,10 +523,11 @@ static id mockNetworking = nil;
 
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
 
-	[[AWSEC2 EC2ForKey:key] assignPrivateIpAddresses:[AWSEC2AssignPrivateIpAddressesRequest new] completionHandler:^(NSError * _Nullable error) {
+	[[AWSEC2 EC2ForKey:key] assignPrivateIpAddresses:[AWSEC2AssignPrivateIpAddressesRequest new] completionHandler:^(AWSEC2AssignPrivateIpAddressesResult* _Nullable response, NSError * _Nullable error) {
         XCTAssertNotNil(error);
         XCTAssertEqualObjects(@"OCMockExpectedNetworkingError", error.domain);
         XCTAssertEqual(8848, error.code);
+        XCTAssertNil(response);
         dispatch_semaphore_signal(semaphore);
     }];
 	
