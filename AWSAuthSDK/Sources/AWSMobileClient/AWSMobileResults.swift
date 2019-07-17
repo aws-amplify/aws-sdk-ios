c9a252d8ab897a90ccaa2547b864e8a422ffc352
Nikita Medvedev <medvednick@yandex.ru>
2019-07-17 21:44:23 +0300

    Added message string property to AWSMobileClientError (#1270)
    * Added message string property to AWSMobileClientError

--- AWSAuthSDK/Sources/AWSMobileClient/AWSMobileResults.swift
+++ AWSAuthSDK/Sources/AWSMobileClient/AWSMobileResults.swift
@@ -154,6 +154,58 @@ extension AWSMobileClient {
     }
 }
 
+extension AWSMobileClientError {
+    /// Underlying error message of `AWSMobileClientError`
+    var message: String {
+        switch self {
+        case .aliasExists(let message),
+             .badRequest(let message),
+             .codeDeliveryFailure(let message),
+             .codeMismatch(let message),
+             .cognitoIdentityPoolNotConfigured(let message),
+             .deviceNotRemembered(let message),
+             .errorLoadingPage(let message),
+             .expiredCode(let message),
+             .expiredRefreshToken(let message),
+             .federationProviderExists(let message),
+             .groupExists(let message),
+             .guestAccessNotAllowed(let message),
+             .idTokenAndAcceessTokenNotIssued(let message),
+             .idTokenNotIssued(let message),
+             .identityIdUnavailable(let message),
+             .internalError(let message),
+             .invalidConfiguration(let message),
+             .invalidLambdaResponse(let message),
+             .invalidOAuthFlow(let message),
+             .invalidParameter(let message),
+             .invalidPassword(let message),
+             .invalidState(let message),
+             .invalidUserPoolConfiguration(let message),
+             .limitExceeded(let message),
+             .mfaMethodNotFound(let message),
+             .notAuthorized(let message),
+             .notSignedIn(let message),
+             .passwordResetRequired(let message),
+             .resourceNotFound(let message),
+             .scopeDoesNotExist(let message),
+             .securityFailed(let message),
+             .softwareTokenMFANotFound(let message),
+             .tooManyFailedAttempts(let message),
+             .tooManyRequests(let message),
+             .unableToSignIn(let message),
+             .unexpectedLambda(let message),
+             .unknown(let message),
+             .userCancelledSignIn(let message),
+             .userLambdaValidation(let message),
+             .userNotConfirmed(let message),
+             .userNotFound(let message),
+             .userPoolNotConfigured(let message),
+             .usernameExists(let message):
+            return message
+        }
+    }
+}
+
 /// Indicates sign in state of the user in the sign in process.
 public enum SignInState: String {
     case unknown = "UNKNOWN"
@@ -230,4 +282,3 @@ public struct UserCodeDeliveryDetails {
         self.attributeName = attributeName
     }
 }
-
