1c41d18f7a6371b492d48e47092631ba863b8fbd
Karthikeyan <kvasukib@ucsd.edu>
2019-07-17 11:46:17 -0700

    Fix SDK Version for Amazon Connect (#1663)

--- AWSConnect/AWSConnectService.m
+++ AWSConnect/AWSConnectService.m
@@ -26,7 +26,7 @@
 #import "AWSConnectResources.h"
 
 static NSString *const AWSInfoConnect = @"Connect";
-NSString *const AWSConnectSDKVersion = @"2.9.9";
+NSString *const AWSConnectSDKVersion = @"2.9.10";
 
 
 @interface AWSConnectResponseSerializer : AWSJSONResponseSerializer
