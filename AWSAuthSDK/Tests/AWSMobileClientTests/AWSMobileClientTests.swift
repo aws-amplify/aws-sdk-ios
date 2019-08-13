133cc68a729934558452a8cf17c50092fd675a36
Jithin Roy <royji@amazon.com>
2019-08-13 15:50:18 -0700

    Added test for unauthenticated session

--- AWSAuthSDK/Tests/AWSMobileClientTests/AWSMobileClientTests.swift
+++ AWSAuthSDK/Tests/AWSMobileClientTests/AWSMobileClientTests.swift
@@ -622,11 +622,10 @@ class AWSMobileClientTests: XCTestCase {
         XCTAssertTrue(AWSMobileClient.sharedInstance().isSignedIn == false, "Expected to return false for isSignedIn")
     }
     
-    /// Test successful sign out with callback and no option
+    /// Test successful sign out with callback and no option on an Authenticated User
     ///
-    /// - Given: An unauthenticated session
+    /// - Given: An authenticated session
     /// - When:
-    ///    - I invoke `signIn` with a completion callback
     ///    - I invoke `signOut` with completion callback
     /// - Then:
     ///    - My `signOut` completion callback is invoked
@@ -646,11 +645,10 @@ class AWSMobileClientTests: XCTestCase {
         wait(for: [signoutExpectation], timeout: 2)
     }
     
-    /// Test successful sign out with options and callback
+    /// Test successful sign out with options and callback on an Authenticated User
     ///
-    /// - Given: An unauthenticated session
+    /// - Given: An authenticated session
     /// - When:
-    ///    - I invoke `signIn` with a completion callback
     ///    - I invoke `signOut` with signoutoptions and completion callback
     /// - Then:
     ///    - My `signOut` completion callback is invoked
@@ -666,6 +664,28 @@ class AWSMobileClientTests: XCTestCase {
         
         let signoutOptions = SignOutOptions(signOutGlobally: true, invalidateTokens: true)
         AWSMobileClient.sharedInstance().signOut(options: signoutOptions) { (error) in
+            XCTAssertTrue(AWSMobileClient.sharedInstance().isSignedIn == false, "Expected to return false for isSignedIn")
+            signoutExpectation.fulfill()
+        }
+        wait(for: [signoutExpectation], timeout: 2)
+    }
+    
+    /// Test successful sign out with callback and no option on a UnAuthenticated user
+    ///
+    /// - Given: An unauthenticated session
+    /// - When:
+    ///    - I invoke `signOut` with completion callback
+    /// - Then:
+    ///    - My `signOut` completion callback is invoked
+    ///    - The user state is `signedOut`
+    ///
+    func testSignOutUnAuthenticatedUser() {
+        let username = "testUser" + UUID().uuidString
+        let signoutExpectation = expectation(description: "Successfully signout for unauthenticated User")
+        signUpAndVerifyUser(username: username)
+        XCTAssertTrue(AWSMobileClient.sharedInstance().isSignedIn == false, "Expected to return false for isSignedIn")
+        sleep(1)
+        AWSMobileClient.sharedInstance().signOut { (error) in
             XCTAssertTrue(AWSMobileClient.sharedInstance().isSignedIn == false, "Expected to return false for isSignedIn")
             signoutExpectation.fulfill()
         }
