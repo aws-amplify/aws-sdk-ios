d2a9c6f03976bb27bb2716d4e44977b2da1c9ce9
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2019-08-19 00:54:10 -0700

    [dynamodb] Update models to latest (#1751)

--- AWSDynamoDB/AWSDynamoDBService.m
+++ AWSDynamoDB/AWSDynamoDBService.m
@@ -281,6 +281,7 @@ static AWSSynchronizedMutableDictionary *_serviceClients = nil;
         headers[@"X-Amz-Target"] = [NSString stringWithFormat:@"%@.%@", targetPrefix, operationName];
         networkingRequest.headers = headers;
         networkingRequest.HTTPMethod = HTTPMethod;
+        networkingRequest.URLString = URLString;
         networkingRequest.requestSerializer = [[AWSJSONRequestSerializer alloc] initWithJSONDefinition:[[AWSDynamoDBResources sharedInstance] JSONObject]
                                                                                                    actionName:operationName];
         networkingRequest.responseSerializer = [[AWSDynamoDBResponseSerializer alloc] initWithJSONDefinition:[[AWSDynamoDBResources sharedInstance] JSONObject]
