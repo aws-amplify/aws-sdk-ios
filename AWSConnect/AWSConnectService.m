0d1a68a58696a37778f26f4dd1d66ff74c519258
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2019-08-20 11:47:48 -0700

    [connect] Update models to latest

--- AWSConnect/AWSConnectService.m
+++ AWSConnect/AWSConnectService.m
@@ -275,6 +275,7 @@ static AWSSynchronizedMutableDictionary *_serviceClients = nil;
         }
 
         networkingRequest.HTTPMethod = HTTPMethod;
+        networkingRequest.URLString = URLString;
         networkingRequest.requestSerializer = [[AWSJSONRequestSerializer alloc] initWithJSONDefinition:[[AWSConnectResources sharedInstance] JSONObject]
                                                                                                    actionName:operationName];
         networkingRequest.responseSerializer = [[AWSConnectResponseSerializer alloc] initWithJSONDefinition:[[AWSConnectResources sharedInstance] JSONObject]
