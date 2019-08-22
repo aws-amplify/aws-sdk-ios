a743c14895c2a82e4095fa5d8bb41a1dce8d2e91
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2019-08-22 06:46:14 -0700

    [logs] Update models to latest (#1796)

--- AWSLogs/AWSLogsService.m
+++ AWSLogs/AWSLogsService.m
@@ -273,7 +273,6 @@ static AWSSynchronizedMutableDictionary *_serviceClients = nil;
         headers[@"X-Amz-Target"] = [NSString stringWithFormat:@"%@.%@", targetPrefix, operationName];
         networkingRequest.headers = headers;
         networkingRequest.HTTPMethod = HTTPMethod;
-        networkingRequest.URLString = URLString;
         networkingRequest.requestSerializer = [[AWSJSONRequestSerializer alloc] initWithJSONDefinition:[[AWSLogsResources sharedInstance] JSONObject]
                                                                                                    actionName:operationName];
         networkingRequest.responseSerializer = [[AWSLogsResponseSerializer alloc] initWithJSONDefinition:[[AWSLogsResources sharedInstance] JSONObject]
