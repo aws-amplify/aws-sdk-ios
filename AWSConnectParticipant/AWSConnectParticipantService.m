ead37684147d8494773a9e8892f22392c8c990b1
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2019-12-11 07:17:01 -0800

    [connectparticipant] Update models to latest

--- AWSConnectParticipant/AWSConnectParticipantService.m
+++ AWSConnectParticipant/AWSConnectParticipantService.m
@@ -25,7 +25,7 @@
 #import "AWSConnectParticipantResources.h"
 
 static NSString *const AWSInfoConnectParticipant = @"ConnectParticipant";
-NSString *const AWSConnectParticipantSDKVersion = @"2.12.2";
+NSString *const AWSConnectParticipantSDKVersion = @"2.12.3";
 
 
 @interface AWSConnectParticipantResponseSerializer : AWSJSONResponseSerializer
