8619867989aa607075cefba6177835664497d756
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2019-12-11 07:22:23 -0800

    [transcribe] Update models to latest

--- AWSTranscribe/AWSTranscribeService.m
+++ AWSTranscribe/AWSTranscribeService.m
@@ -25,7 +25,7 @@
 #import "AWSTranscribeResources.h"
 
 static NSString *const AWSInfoTranscribe = @"Transcribe";
-NSString *const AWSTranscribeSDKVersion = @"2.12.2";
+NSString *const AWSTranscribeSDKVersion = @"2.12.3";
 
 
 @interface AWSTranscribeResponseSerializer : AWSJSONResponseSerializer
