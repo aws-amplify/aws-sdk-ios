83928dcf85d0ab71d5338c1dc8418f65ef90b158
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2019-12-11 07:18:01 -0800

    [email] Update models to latest

--- AWSSES/AWSSESService.m
+++ AWSSES/AWSSESService.m
@@ -25,7 +25,7 @@
 #import "AWSSESResources.h"
 
 static NSString *const AWSInfoSES = @"SES";
-NSString *const AWSSESSDKVersion = @"2.12.2";
+NSString *const AWSSESSDKVersion = @"2.12.3";
 
 
 @interface AWSSESResponseSerializer : AWSXMLResponseSerializer
