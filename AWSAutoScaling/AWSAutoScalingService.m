c4cc637e14d1a04a3ccc1de5c42eaafac418f639
awsmobilesdk <circleci@ea908549885d>
2020-03-26 20:05:50 +0000

    [bump version 2.13.1]

--- AWSAutoScaling/AWSAutoScalingService.m
+++ AWSAutoScaling/AWSAutoScalingService.m
@@ -25,7 +25,7 @@
 #import "AWSAutoScalingResources.h"
 
 static NSString *const AWSInfoAutoScaling = @"AutoScaling";
-NSString *const AWSAutoScalingSDKVersion = @"2.13.0";
+NSString *const AWSAutoScalingSDKVersion = @"2.13.1";
 
 
 @interface AWSAutoScalingResponseSerializer : AWSXMLResponseSerializer
--- AWSTranslate/AWSTranslateService.m
+++ AWSTranslate/AWSTranslateService.m
@@ -25,7 +25,7 @@
 #import "AWSTranslateResources.h"
 
 static NSString *const AWSInfoTranslate = @"Translate";
-NSString *const AWSTranslateSDKVersion = @"2.13.0";
+NSString *const AWSTranslateSDKVersion = @"2.13.1";
 
 
 @interface AWSTranslateResponseSerializer : AWSJSONResponseSerializer
