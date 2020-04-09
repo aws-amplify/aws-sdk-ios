0098afc1499c23330149178c46f6e93ca6e35e11
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2020-04-09 15:01:14 -0700

    [bump version 2.13.2] (#2422)
    Co-authored-by: awsmobilesdk <circleci@ea908549885d>

--- AWSAPIGateway/AWSAPIGatewayClient.m
+++ AWSAPIGateway/AWSAPIGatewayClient.m
@@ -23,7 +23,7 @@ NSString *const AWSAPIGatewayErrorHTTPHeaderFieldsKey = @"HTTPHeaderFields";
 
 static NSString *const AWSAPIGatewayAPIKeyHeader = @"x-api-key";
 
-NSString *const AWSAPIGatewaySDKVersion = @"2.13.1";
+NSString *const AWSAPIGatewaySDKVersion = @"2.13.2";
 
 static int defaultChunkSize = 1024;
 
--- AWSSimpleDB/AWSSimpleDBService.m
+++ AWSSimpleDB/AWSSimpleDBService.m
@@ -25,7 +25,7 @@
 #import "AWSSimpleDBResources.h"
 
 static NSString *const AWSInfoSimpleDB = @"SimpleDB";
-NSString *const AWSSimpleDBSDKVersion = @"2.13.1";
+NSString *const AWSSimpleDBSDKVersion = @"2.13.2";
 
 
 @interface AWSSimpleDBResponseSerializer : AWSXMLResponseSerializer
