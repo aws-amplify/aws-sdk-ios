678bc917b0c277b3c19281afd0dcbe8774a9c442
Jithin Roy <royji@amazon.com>
2020-06-29 16:06:16 -0700

    Added fallback to default

--- AWSAuthSDK/Sources/AWSMobileClient/AWSUserPoolOperationsHandler.swift
+++ AWSAuthSDK/Sources/AWSMobileClient/AWSUserPoolOperationsHandler.swift
@@ -75,22 +75,30 @@ AWSCognitoUserPoolInternalDelegate {
         }
     }
 
-    private func getUserPoolClient() -> AWSCognitoIdentityUserPool? {
+    private func getUserPoolClient() -> AWSCognitoIdentityUserPool {
         
         guard let serviceConfig = UserPoolOperationsHandler.serviceConfiguration?.userPoolServiceConfiguration else {
             return AWSCognitoIdentityUserPool.default()
         }
+
+        // Check if a AWSCognitoIdentityUserPool is already registered with the given key.
         let clientKey = "CognitoUserPoolKey"
         if let client = AWSCognitoIdentityUserPool.init(forKey: clientKey) {
             return client
         }
 
+        // If the AWSCognitoIdentityUserPool is not registered, register it and then return the same object.
         let serviceInfo = AWSInfo.default().defaultServiceInfo("CognitoUserPool")
         let userPoolConfig = AWSCognitoIdentityUserPool.buildConfiguration(serviceInfo)
         AWSCognitoIdentityUserPool.register(with: serviceConfig,
                                             userPoolConfiguration: userPoolConfig,
                                             forKey: clientKey)
-        return AWSCognitoIdentityUserPool.init(forKey: clientKey)
+        if let client =  AWSCognitoIdentityUserPool.init(forKey: clientKey) {
+            return client
+        }
+
+        /// Fall back to the default client if the client was not registered correctly.
+        return AWSCognitoIdentityUserPool.default()
     }
     
     internal func startPasswordAuthentication() -> AWSCognitoIdentityPasswordAuthentication {
