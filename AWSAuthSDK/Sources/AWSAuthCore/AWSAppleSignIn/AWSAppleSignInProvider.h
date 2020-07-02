1ca92ace66cb73d7ea2336603bd17dbae311c68a
Jithin Roy <royji@amazon.com>
2020-07-01 18:34:47 -0700

    Updated doc comment

--- AWSAuthSDK/Sources/AWSAuthCore/AWSAppleSignIn/AWSAppleSignInProvider.h
+++ AWSAuthSDK/Sources/AWSAuthCore/AWSAppleSignIn/AWSAppleSignInProvider.h
@@ -1,9 +1,16 @@
 //
-//  AWSAppleSignInProvider.h
-//  AWSAuthCore
+// Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
-//  Created by Roy, Jithin on 6/28/20.
-//  Copyright © 2020 Amazon Web Services. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
