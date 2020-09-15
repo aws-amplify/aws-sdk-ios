845030b078135b9c4ea1c458d0834f9f4d8f59e2
Richard19950618 <48600426+DongQuanRui@users.noreply.github.com>
2020-09-15 16:51:34 -0700

    (AWSAuthSDK) Upgrade Facebook SDK to v6.5.2 (#2990)
    * the sample app works, not yet done integration tests
    
    * Update AWSFacebookSignIn.podspec

--- AWSAuthSDK/Dependencies/FacebookHeaders/FBSDKGraphErrorRecoveryProcessor.h
+++ AWSAuthSDK/Dependencies/FacebookHeaders/FBSDKGraphErrorRecoveryProcessor.h
@@ -16,9 +16,17 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#import "TargetConditionals.h"
+
+#if !TARGET_OS_TV
+
 #import <Foundation/Foundation.h>
 
+#ifdef BUCK
+#import <FBSDKCoreKit/FBSDKConstants.h>
+#else
 #import "FBSDKConstants.h"
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -112,3 +120,5 @@ NS_SWIFT_UNAVAILABLE("")
 @end
 
 NS_ASSUME_NONNULL_END
+
+#endif
--- AWSAuthSDK/Dependencies/FacebookHeaders/FBSDKGraphRequest.h
+++ AWSAuthSDK/Dependencies/FacebookHeaders/FBSDKGraphRequest.h
@@ -18,7 +18,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import <FBSDKGraphRequestConnection.h>
+#import "FBSDKGraphRequestConnection.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
