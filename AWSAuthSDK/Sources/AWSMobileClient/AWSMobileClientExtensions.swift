03a71ef03ca643c77c08e8ba259fae70bf051037
Victor Rolando Sanchez Jara <roli.s91@gmail.com>
2020-10-26 18:32:00 +0000

    Fix updated claims that contain special characters, such as chinese c… (#2928)
    * Fix updated claims that contain special characters, such as chinese chars, by replacing _ and - with / and +
    
    * Update code to match existing implementation in amplify-ios/AmplifyPlugins/Core/AWSPluginsCore/Auth/AWSAuthService.swift

--- AWSAuthSDK/Sources/AWSMobileClient/AWSMobileClientExtensions.swift
+++ AWSAuthSDK/Sources/AWSMobileClient/AWSMobileClientExtensions.swift
@@ -36,22 +36,25 @@ public struct SessionToken {
                 return nil
             }
             let claims = tokenSplit[1]
+                .replacingOccurrences(of: "-", with: "+")
+                .replacingOccurrences(of: "_", with: "/")
             
             let paddedLength = claims.count + (4 - (claims.count % 4)) % 4
             //JWT is not padded with =, pad it if necessary
             let updatedClaims = claims.padding(toLength: paddedLength, withPad: "=", startingAt: 0)
-            let claimsData = Data.init(base64Encoded: updatedClaims, options: .ignoreUnknownCharacters)
-            
-            guard claimsData != nil else {
+            let encodedData = Data(base64Encoded: updatedClaims, options: .ignoreUnknownCharacters)
+
+            guard let claimsData = encodedData else {
                 print("Cannot get claims in `Data` form. Token is not valid base64 encoded string.")
                 return nil
             }
-            let jsonObject = try? JSONSerialization.jsonObject(with: claimsData!, options: [])
-            guard jsonObject != nil else {
+            
+            let jsonObject = try? JSONSerialization.jsonObject(with: claimsData, options: [])
+            guard let convertedDictionary = jsonObject as? [String: AnyObject] else {
                 print("Cannot get claims in `Data` form. Token is not valid JSON string.")
                 return nil
             }
-            return jsonObject as? [String: AnyObject]
+            return convertedDictionary
         }
     }
     
