a7fb32d22d9591aed23feacab2b72b16cb5f3a50
Jithin Roy <51138777+royjit@users.noreply.github.com>
2020-11-04 11:47:02 -0800

    Update AWSAuthSDK/Sources/AWSMobileClient/AWSMobileClientExtensions.swift
    Co-authored-by: Tim Schmelter <schmelte@amazon.com>

--- AWSAuthSDK/Sources/AWSMobileClient/AWSMobileClientExtensions.swift
+++ AWSAuthSDK/Sources/AWSMobileClient/AWSMobileClientExtensions.swift
@@ -236,7 +236,7 @@ extension AWSMobileClient {
                     self.performUserPoolSuccessfulSignInTasks(session: result)
                     let tokenString = result.idToken!.tokenString
                     
-                    // Invoke credentials inorder to refresh the id token before returning signedin
+                    // Invoke `credentials` in order to refresh the id token before returning signedIn
                     self.internalCredentialsProvider?.credentials(withCancellationToken: self.credentialsFetchCancellationSource).continueWith { _ in
                         self.mobileClientStatusChanged(userState: .signedIn,
                                                        additionalInfo: [self.ProviderKey:self.userPoolClient!.identityProviderName,
