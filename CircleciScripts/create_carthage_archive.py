98bda86a9a7008ab32b29ee0392b413e67a39112
Jithin Roy <51138777+royjit@users.noreply.github.com>
2021-01-25 10:37:53 -0800

    chore: CircleCI script to generate XCFrameworks (#3368)
    XCFramework is the recommended medium to distribute framework. This PR removes the old way of generating framework and implements XCFramework.

--- CircleciScripts/create_carthage_archive.py
+++ CircleciScripts/create_carthage_archive.py
@@ -1,43 +1,13 @@
 import os
-import sys
+import shutil
 
-from framework_list import frameworks
-from functions import log, run_command
+from functions import log
 
-EXCLUDE_FROM_CARTHAGE = [
-    # This isn't a real framework
-    "AWSiOSSDKv2",
-    # Legacy frameworks not built or packaged
-    "AWSAuth",
-]
-
-
-def is_framework_included(framework):
-    return framework not in EXCLUDE_FROM_CARTHAGE
-
+XCFRAMEWORK_PATH = "./output/XCF/"
 
 project_dir = os.getcwd()
 log(f"Creating Carthage archive in {project_dir}")
 
-framework_file_name = "aws-sdk-ios-carthage.framework.zip"
+framework_file_name = "aws-sdk-ios-carthage.framework"
 
-filtered_frameworks = list(filter(is_framework_included, frameworks))
-
-cmd = [
-    "carthage",
-    "archive",
-    "--project-directory",
-    project_dir,
-    "--output",
-    framework_file_name,
-] + filtered_frameworks
-
-# Carthage builds each SDK as part of its process, so set a generous timeout for this process
-(exit_code, out, err) = run_command(cmd, keepalive_interval=300, timeout=7200)
-
-if exit_code == 0:
-    log(f"Created Carthage archive {framework_file_name}")
-else:
-    log(f"Could not create Carthage archive: output: {out}; error: {err}")
-
-sys.exit(exit_code)
+shutil.make_archive(framework_file_name, 'zip', XCFRAMEWORK_PATH)
