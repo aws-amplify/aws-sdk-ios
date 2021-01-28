b237568162521c9e802231f94da135ef0dcb1101
Jithin Roy <51138777+royjit@users.noreply.github.com>
2021-01-28 11:53:38 -0800

    chore: Update the xcframework script to cache the right folder (#3391)

--- CircleciScripts/create_carthage_archive.py
+++ CircleciScripts/create_carthage_archive.py
@@ -3,11 +3,12 @@ import shutil
 
 from functions import log
 
-XCFRAMEWORK_PATH = "./output/XCF/"
-
 project_dir = os.getcwd()
 log(f"Creating Carthage archive in {project_dir}")
 
+xcframework_path = f"{project_dir}/xcframeworks/output/XCF/"
+log(f"XCFramework from -  {xcframework_path}")
+
 framework_file_name = "aws-sdk-ios-carthage.framework"
 
-shutil.make_archive(framework_file_name, 'zip', XCFRAMEWORK_PATH)
+shutil.make_archive(framework_file_name, 'zip', xcframework_path)
