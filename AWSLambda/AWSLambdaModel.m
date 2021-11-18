118295dc9502edd3dab61d245cde0df753b85055
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2021-11-18 19:26:08 +0000

    feat(AWSLambda): update models to latest

--- AWSLambda/AWSLambdaModel.m
+++ AWSLambda/AWSLambdaModel.m
@@ -3141,6 +3141,12 @@ NSString *const AWSLambdaErrorDomain = @"com.amazonaws.AWSLambdaErrorDomain";
         if ([value caseInsensitiveCompare:@"VIRTUAL_HOST"] == NSOrderedSame) {
             return @(AWSLambdaSourceAccessTypeVirtualHost);
         }
+        if ([value caseInsensitiveCompare:@"CLIENT_CERTIFICATE_TLS_AUTH"] == NSOrderedSame) {
+            return @(AWSLambdaSourceAccessTypeClientCertificateTlsAuth);
+        }
+        if ([value caseInsensitiveCompare:@"SERVER_ROOT_CA_CERTIFICATE"] == NSOrderedSame) {
+            return @(AWSLambdaSourceAccessTypeServerRootCaCertificate);
+        }
         return @(AWSLambdaSourceAccessTypeUnknown);
     } reverseBlock:^NSString *(NSNumber *value) {
         switch ([value integerValue]) {
@@ -3156,6 +3162,10 @@ NSString *const AWSLambdaErrorDomain = @"com.amazonaws.AWSLambdaErrorDomain";
                 return @"SASL_SCRAM_256_AUTH";
             case AWSLambdaSourceAccessTypeVirtualHost:
                 return @"VIRTUAL_HOST";
+            case AWSLambdaSourceAccessTypeClientCertificateTlsAuth:
+                return @"CLIENT_CERTIFICATE_TLS_AUTH";
+            case AWSLambdaSourceAccessTypeServerRootCaCertificate:
+                return @"SERVER_ROOT_CA_CERTIFICATE";
             default:
                 return nil;
         }
