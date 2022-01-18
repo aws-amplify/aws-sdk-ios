cec7e778eed8a7f30beb4e7edff9be83f215c599
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2022-01-18 13:56:42 -0800

    feat(AWSRekognition): update models to latest (#3957)

--- AWSRekognition/AWSRekognitionModel.m
+++ AWSRekognition/AWSRekognitionModel.m
@@ -1,5 +1,5 @@
 //
-// Copyright 2010-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2010-2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License").
 // You may not use this file except in compliance with the License.
@@ -2063,6 +2063,7 @@ NSString *const AWSRekognitionErrorDomain = @"com.amazonaws.AWSRekognitionErrorD
              @"externalImageId" : @"ExternalImageId",
              @"faceId" : @"FaceId",
              @"imageId" : @"ImageId",
+             @"indexFacesModelVersion" : @"IndexFacesModelVersion",
              };
 }
 
