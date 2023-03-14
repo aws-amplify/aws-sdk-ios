eafda25ab0865a6f9730c25b19accf7f5f9e28b7
Jithin Roy <51138777+royjit@users.noreply.github.com>
2023-03-14 11:45:03 -0700

    Added test to branch

--- AWSCoreTests/AWSCognitoCredentialsProviderConcurrencyTests.m
+++ AWSCoreTests/AWSCognitoCredentialsProviderConcurrencyTests.m
@@ -80,6 +80,7 @@ NSString *TestsIdentityPoolId = nil;
 }
 
 - (void)testMultiThreadedWithEnhancedFlow {
+
     AWSCognitoCredentialsProviderHelper *identityProvider = [[AWSCognitoCredentialsProviderHelper alloc]
                                                              initWithRegionType: self.region
                                                              identityPoolId:TestsIdentityPoolId
