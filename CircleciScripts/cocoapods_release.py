b78e90ed3b0e1740dca68576c5410ae472a3b260
Jithin Roy <51138777+royjit@users.noreply.github.com>
2023-04-28 12:21:18 -0700

    (chore): Add retry for cocoapods pod release [skip ci]

--- CircleciScripts/cocoapods_release.py
+++ CircleciScripts/cocoapods_release.py
@@ -2,11 +2,9 @@ import sys
 
 from framework_list import frameworks
 from functions import log, run_command
+import time
 
-log("Publishing CocoaPods")
-
-for framework in frameworks:
-    log(f"Publishing {framework}")
+def publish(framework: str, retryAttempt: int = 0):
 
     # Most pods take a few minutes to build, and a few seconds to push to trunk. However, the
     # AWSiOSSDK podspec can take a long time to build, since it builds each dependent pod as
@@ -16,6 +14,19 @@ for framework in frameworks:
         keepalive_interval=300,
         timeout=3600,
     )
+
+    if retryAttempt < 3 and exit_code != 0 and "Unable to accept duplicate entry for" not in str(out):
+        retryAttempt += 1
+        log(f"Retrying {framework}")
+        return publish(framework=framework, retryAttempt=retryAttempt)
+    return (exit_code, out, err)
+
+log("Publishing CocoaPods")
+
+for framework in frameworks:
+    log(f"Publishing {framework}")
+
+    (exit_code, out, err) = publish(framework=framework)
 
     if exit_code != 0 and "Unable to accept duplicate entry for" in str(out):
         log(f"Already published {framework}")
