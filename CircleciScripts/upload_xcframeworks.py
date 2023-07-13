9bf10f349c3bbdae836e658a97ffdf09a95b8747
Di Wu <im@wudi.me>
2023-07-13 13:37:21 -0700

    add release workflow in github action

--- CircleciScripts/upload_xcframeworks.py
+++ CircleciScripts/upload_xcframeworks.py
@@ -6,10 +6,10 @@ import logging
 
 from functions import setup_logging
 from framework_list import xcframeworks
-from semver_util import validate_version 
+from semver_util import validate_version
 from botocore.exceptions import ClientError
 
-def upload_file(file_name, bucket, object_name): 
+def upload_file(file_name, bucket, object_name):
     s3_client = boto3.client('s3')
     try:
         s3_client.upload_file(file_name, bucket, object_name)
@@ -17,13 +17,12 @@ def upload_file(file_name, bucket, object_name):
         logging.error(e)
         sys.exit(e.response['Error']['Code'])
 
-setup_logging()  
+setup_logging()
 project_dir = os.getcwd()
 archive_path = f"{project_dir}/xcframeworks/output/archives"
 
 bucket = sys.argv[1]
 version = sys.argv[2]
-aws_profile = sys.argv[3]
 
 if validate_version(version) == False:
     logging.error("Version is invalid, exiting")
@@ -33,13 +32,7 @@ if not re.match("^[a-zA-Z0-9-]*$", bucket):
     logging.error("Bucket is invalid, exiting")
     sys.exit(1)
 
-if aws_profile is None:
-    logging.error("AWS profile should not be none")
-    sys.exit(1)
-
 logging.info(f"Uploading xcframeworks from {archive_path}")
-
-boto3.setup_default_session(profile_name=aws_profile)
 
 for framework in xcframeworks:
     filename = f"{framework}-{version}.zip"
