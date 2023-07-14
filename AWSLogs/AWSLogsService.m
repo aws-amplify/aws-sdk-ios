2e8923e3d5c34706cc99af3b89ad6411980095b6
runner <runner@Mac-1689305447168.local>
2023-07-14 03:38:29 +0000

    [bump version "$RELEASE_VERSION"]

--- AWSLogs/AWSLogsService.m
+++ AWSLogs/AWSLogsService.m
@@ -25,7 +25,7 @@
 #import "AWSLogsResources.h"
 
 static NSString *const AWSInfoLogs = @"Logs";
-NSString *const AWSLogsSDKVersion = @"2.33.1";
+NSString *const AWSLogsSDKVersion = @"2.33.2";
 
 
 @interface AWSLogsResponseSerializer : AWSJSONResponseSerializer
