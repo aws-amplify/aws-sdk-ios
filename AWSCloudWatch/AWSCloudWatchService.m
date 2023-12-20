4fff88bad6c738ec6a6eca5bd77d18f978434e1b
aws-amplify-ops <aws-amplify-ops@amazon.com>
2023-12-20 19:58:55 +0000

    [bump version 2.33.7]

--- AWSCloudWatch/AWSCloudWatchService.m
+++ AWSCloudWatch/AWSCloudWatchService.m
@@ -26,7 +26,7 @@
 #import "AWSCloudWatchResources.h"
 
 static NSString *const AWSInfoCloudWatch = @"CloudWatch";
-NSString *const AWSCloudWatchSDKVersion = @"2.33.6";
+NSString *const AWSCloudWatchSDKVersion = @"2.33.7";
 
 
 @interface AWSCloudWatchResponseSerializer : AWSXMLResponseSerializer
--- AWSLambda/AWSLambdaService.m
+++ AWSLambda/AWSLambdaService.m
@@ -26,7 +26,7 @@
 #import "AWSLambdaRequestRetryHandler.h"
 
 static NSString *const AWSInfoLambda = @"Lambda";
-NSString *const AWSLambdaSDKVersion = @"2.33.6";
+NSString *const AWSLambdaSDKVersion = @"2.33.7";
 
 
 @interface AWSLambdaResponseSerializer : AWSJSONResponseSerializer
