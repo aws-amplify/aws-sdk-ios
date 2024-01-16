72be6ce6f1f2f3710a971792a25299b261a2b943
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2024-01-16 05:08:06 +0000

    feat(AWSLocation): update models to latest

--- AWSLocation/AWSLocationModel.m
+++ AWSLocation/AWSLocationModel.m
@@ -1,5 +1,5 @@
 //
-// Copyright 2010-2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2010-2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License").
 // You may not use this file except in compliance with the License.
@@ -3299,6 +3299,7 @@ NSString *const AWSLocationErrorDomain = @"com.amazonaws.AWSLocationErrorDomain"
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
 	return @{
+             @"customLayers" : @"CustomLayers",
              @"politicalView" : @"PoliticalView",
              @"style" : @"Style",
              };
@@ -3314,6 +3315,7 @@ NSString *const AWSLocationErrorDomain = @"com.amazonaws.AWSLocationErrorDomain"
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
 	return @{
+             @"customLayers" : @"CustomLayers",
              @"politicalView" : @"PoliticalView",
              };
 }
