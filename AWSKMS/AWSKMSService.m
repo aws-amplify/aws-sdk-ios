357c08e8ad817b282631ca1af60317612d524bfb
aws-amplify-ops <aws-amplify-ops@amazon.com>
2024-03-21 13:37:09 +0000

    [bump version 2.34.1]

--- AWSKMS/AWSKMSService.m
+++ AWSKMS/AWSKMSService.m
@@ -25,7 +25,7 @@
 #import "AWSKMSResources.h"
 
 static NSString *const AWSInfoKMS = @"KMS";
-NSString *const AWSKMSSDKVersion = @"2.34.0";
+NSString *const AWSKMSSDKVersion = @"2.34.1";
 
 
 @interface AWSKMSResponseSerializer : AWSJSONResponseSerializer
